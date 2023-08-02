ef66e74c2da5c66be30883de203cfb0e1eed64f9
Felipe Azzolino Varella <felipe_azzolinovarella@hotmail.com>
2023-08-02 18:17:21 -0300

    Atualizando simulacao base

--- scripts/get_params.m
+++ scripts/get_params.m
@@ -25,6 +25,8 @@ fprintf('Definindo matrizes do sistema...\n')
 % Sistema saudavel (A sem variaçao)
 A = [0 1 0 -1; -ks/mv -bs/mv 0 bs/mv; 0 0 0 1; ks/mr bs/mr -kp/mr -(bs+bp)/mr];
 B1 = [0; 0; -1; bp/mr];
+B2 = [0; 1/mv; 0; -1/mr];
+B = [B1 B2];
 C = [1 0 0 0; 0 1 0 0];
 % C = [0 0 1 0; 0 0 0 1];  % APENAS PARA TESTE! COMENTAR
 D = 0;
@@ -39,6 +41,10 @@ A_c2 = [0 1 0 -1; -ks/mv -bs_/mv 0 bs_/mv; 0 0 0 1; ks/mr bs_/mr -kp/mr -(bs_+bp
 %%% Caso 3: Perda da rigidez da mola da suspensao de 50% 
 ks_ = 0.5*ks;
 A_c3 = [0 1 0 -1; -ks_/mv -bs/mv 0 bs/mv; 0 0 0 1; ks_/mr bs/mr -kp/mr -(bs+bp)/mr];
+%%% Caso 4 (teste): Perda de amortecimento da suspensao e da rigidez da mola
+ks_ = 0;
+bs_ = 0;
+A_c4 = [0 1 0 -1; -ks_/mv -bs/mv 0 bs/mv; 0 0 0 1; ks_/mr bs/mr -kp/mr -(bs+bp)/mr];
 
 %% Projeto do observador
 
@@ -58,16 +64,16 @@ Ko2_luenberger = place(A', C_obs2', p_obs)';
 
 %%% UIO - Heloi
 %%%% Polos desejados para o observador  --- ENTENDER PORQUE ESTA DANDO ERRADO E REFAZER
-% p_obs = 4*p;
-% %%%% Observador 1
-% E_obs1 = -B1*pinv(C_obs1*B1);
+p_obs = 4*p;
+%%%% Observador 1
+% E_obs1 = -B*pinv(C_obs1*B);
 % H_obs1 = eye(length(E_obs1)) + E_obs1*C_obs1;
 % A1_obs1 = H_obs1*A;
 % Ko1_uio = place(A1_obs1', C_obs1', p_obs)';
 % N_obs1 = A1_obs1 - Ko1_uio*C_obs1;
 % L_obs1 = Ko1_uio - N_obs1*E_obs1;
-% %%%% Observador 2
-% E_obs2 = -B1*pinv(C_obs2*B1);
+% %%% Observador 2
+% E_obs2 = -B*pinv(C_obs2*B);
 % H_obs2 = eye(length(E_obs2)) + E_obs2*C_obs2;
 % A1_obs2 = H_obs2*A;
 % Ko2_uio = place(A1_obs2', C_obs2', p_obs)';
@@ -77,8 +83,8 @@ Ko2_luenberger = place(A', C_obs2', p_obs)';
 %%% UIO - Chen
 %%%% Polos desejados para o observador  --- ENTENDER PORQUE ESTA DANDO ERRADO E REFAZER
 % p_obs = 4*p;
-% if rank(C_obs1*B1) == rank(B1)
-%     H_obs1 = B1*inv((C_obs1*B1)'*(C_obs1*B1))*(C_obs1*B1)';
+% if rank(C_obs1*B) == rank(B)
+%     H_obs1 = B*inv((C_obs1*B)'*(C_obs1*B))*(C_obs1*B)';
 %     T_obs1 = eye(length(H_obs1)) - H_obs1*C_obs1;
 %     A1_obs1 = T_obs1*A;
 %     K1_obs1 = place(A1_obs1', C_obs1', p_obs)';
@@ -102,22 +108,20 @@ f0 = 1/16;
 T0 = 1/f0;
 k1 = 0*(1/f0);
 k2 = 6*(1/f0);
-dt = 2.5E-6;  % Para que a gente consiga visualizar bem
+dt = 2.5E-5;  % Para que a gente consiga visualizar bem
 t_range = 0:dt:T0;
 N = 2;  % Numero de periodos
+% N = 1;  % Numero de periodos
 
 % Sinal gerado
-w = [];
-t = [];
-for i=1:N
-    tt = (i-1)*T0 + t_range;
-    ww = chirp(As, f0, k1, k2, t_range);
+w = chirp(As, f0, k1, k2, t_range);
+t = t_range;
+for i=2:N
+    tt = (i-1)*T0 + t_range(2:end);
+    ww = chirp(As, f0, k1, k2, t_range(2:end));
     w = [w, ww];
     t = [t, tt];
 end
-
-% Apenas para testes (COMENTAR DEPOIS)
-% w(t >= 5) = 0;
 
 % Exportando para arquivo
 wt = timeseries(w, t);
@@ -127,13 +131,29 @@ save('../data/chirp_data.mat', 'wt', '-v7.3')
 
 fprintf('Gerando sinal de ruido...\n')
 
-% W = 10;  % Variancia - Como determinar?
-V = [1E-6 0; 0 1E-3];  % Variancia - Como determinar?
+% Pegando y e os valores maximos
+sys = ss(A, B1, C, D);  % B1 pois queremos estudar apenas w(t)
+y = lsim(sys, w, t);
+y1 = y(:,1);
+y2 = y(:,2);
 
-% w_ = sqrt(W)*randn(1, length(t));
-v_ = sqrt(V)*randn(2, length(t));
+% Procedimento para gerar matriz de variancia
+%%% Usando % do maximo
+% pmax = 5/100;
+% v_ry1 = pmax*max(y1);
+% v_ry2 = pmax*max(y2);
+%%% Usando SNR
+SNR = 20;  % 20dB --> Sinal eh 100x mais potente que o ruido
+% SNR = 30;  % 30dB --> Sinal eh 1000x mais potente que o ruido
+s_ry1 = std(y1)/(10^(SNR/20));
+s_ry2 = std(y2)/(10^(SNR/20));
+v_ry1 = s_ry1^2;
+v_ry2 = s_ry2^2;
+
+% Gerando as variancias
+V = [v_ry1 0; 0 v_ry2];  % Variancia - Como determinar? Via SNR ou %Max?
+
+v = sqrt(V)*randn(2, length(t));
  
-% w_t = timeseries(w_, t);
-% save('../data/process_noise.mat', 'w_t', '-v7.3')
-v_t = timeseries(v_, t);
-save('../data/measurement_noise.mat', 'v_t', '-v7.3')
+vt = timeseries(v, t);
+save('../data/measurement_noise.mat', 'vt', '-v7.3')
